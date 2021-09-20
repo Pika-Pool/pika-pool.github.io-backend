68a3fcbda96aa3d6591305816ff68c6c5c2ae9fc
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2021-09-20 19:40:50 +0530

    fix cors error on localhost and send message list on srispecial

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 if (process.env.NODE_ENV !== 'production') require('dotenv-safe').config();
 
 const express = require('express');
@@ -5,42 +7,43 @@ const cors = require('cors');
 
 const app = express();
 
-const whitelist = process.env.CORS_WHITELIST.split(',').map(
-	url => new URL(url)
-);
+const whitelist = process.env.CORS_WHITELIST.split(',').map(url => {
+	try {
+		return new URL(url);
+	} catch (err) {
+		return url;
+	}
+});
 const corsOptions = {
 	origin: (origin, cb) => {
-		if (
-			whitelist[0] === '*' ||
-			whitelist.findIndex(url => origin === url.origin) !== -1
-		)
+		if (whitelist.findIndex(url => url === '*' || origin === url.origin) !== -1)
 			cb(null, true);
 		else cb(new Error('CORS not allowed'));
 	},
-	methods: ['POST'],
+	preflightContinue: true,
+	methods: ['POST', 'GET', 'OPTIONS'],
 };
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const messages = [];
+const messageList = [];
 
 app.post('/contact', (req, res) => {
 	const { name, email, message } = req.body;
 
 	console.log({ name, email, message });
 	if (
-		name != null &&
+		name != undefined &&
 		name.length >= 1 &&
-		email != null &&
+		email != undefined &&
 		/^\S+@\S+$/.test(email) &&
-		message != null &&
+		message != undefined &&
 		message.length >= 10 &&
 		message.length <= 5000
 	) {
-		messages.push({ name, email, message });
+		messageList.push({ name, email, message });
 		res.send();
 		return;
 	}
@@ -54,7 +57,7 @@ app.post('/contact', (req, res) => {
 
 app.get('/contact', (req, res) => {
 	const { srispecial } = req.query;
-	if (srispecial === process.env.SRI_SPECIAL) res.json(message);
+	if (srispecial === process.env.SRI_SPECIAL) res.json(messageList);
 	else res.status(404).send('<h1>404</h1>');
 });
 
